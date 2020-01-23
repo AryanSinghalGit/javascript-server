e4a708ff3da7720fa57195bfbc1375cc66c5b8b2
Aryan Singhal <aryan.singhal@lapr247.successivetech.com>
2020-01-23 17:45:31 +0530

    feat: Add Package extra

---
+++ extra/patterns/equilateral.js
@@ -0,0 +1,24 @@
+n=process.argv[2]
+equilateral(n)
+function equilateral(n)
+{
+    
+ 
+for(let i=0;i<n;i++)
+{
+    let str=""
+    for(let k=0;k<n-i-1;k++)
+    {
+        str=str+" "
+    }
+    for(let j=0;j<=i;j++)
+    {
+        
+        str=str+"* "
+    }
+    console.log(str)
+     
+}
+
+}
+

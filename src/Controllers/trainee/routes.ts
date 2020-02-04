185517fb207d1b9cae929c78d25b381d90966b84
Aryan Singhal <aryan.singhal@lapr247.successivetech.com>
2020-02-04 18:26:25 +0530

    feat: 🎸 Authentication and Authorization
    ✅ Closes: 40009

--- src/Controllers/trainee/routes.ts
+++ src/Controllers/trainee/routes.ts
@@ -4,9 +4,9 @@ import { validationHandler, authMiddleWare } from '../../libs/routes' ;
 import { default as validation } from './validation' ;
 const traineeRouter: Router = Router();
 traineeRouter.route('/')
-    .get(validationHandler(validation.get), Controller.create)
+    .get(authMiddleWare('traineeModule', 'read'), validationHandler(validation.get), Controller.create)
     .post(authMiddleWare('traineeModule', 'read'), validationHandler(validation.create), Controller.list);
 traineeRouter.route('/:id')
-    .delete(validationHandler(validation.delete), Controller.delete)
-    .put(validationHandler(validation.update), Controller.update);
+    .delete(authMiddleWare('traineeModule', 'read'), validationHandler(validation.delete), Controller.delete)
+    .put(authMiddleWare('traineeModule', 'read'), validationHandler(validation.update), Controller.update);
 export default traineeRouter;

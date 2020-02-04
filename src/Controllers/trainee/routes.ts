496bbb6e22a720ae88a66417e90a482df32e437a
Aryan Singhal <aryan.singhal@lapr247.successivetech.com>
2020-02-04 17:34:11 +0530

    feat: 🎸 Authentication and Authorization
    ✅ Closes: 40009

--- src/Controllers/trainee/routes.ts
+++ src/Controllers/trainee/routes.ts
@@ -4,8 +4,8 @@ import { validationHandler, authMiddleWare } from '../../libs/routes' ;
 import { default as validation } from './validation' ;
 const traineeRouter: Router = Router();
 traineeRouter.route('/')
-    .get(authMiddleWare('getUser', 'read'), validationHandler(validation.get), Controller.create)
-    .post(validationHandler(validation.create), Controller.list);
+    .get(validationHandler(validation.get), Controller.create)
+    .post(authMiddleWare('traineeModule', 'read'), validationHandler(validation.create), Controller.list);
 traineeRouter.route('/:id')
     .delete(validationHandler(validation.delete), Controller.delete)
     .put(validationHandler(validation.update), Controller.update);
--- src/libs/routes/authMiddleWare.ts
+++ src/libs/routes/authMiddleWare.ts
@@ -1,8 +1,34 @@
 import * as jwt from 'jsonwebtoken';
+import { default as config } from '../../config/configuration';
+import hasPermission from '../hasPermission';
 export default (module, permissionType) => (req, res, next) => {
-    const token = req.body.token;
-    const decodedUser = jwt.verify(token, key);
-    if(!decodedUser){
-        return next({});
+    console.log('------------AUTHMIDDLEWARE------------', module, permissionType);
+    try {
+        const token = req.body.Authorization;
+        const { Key } = config;
+        const decodedUser = jwt.verify(token, Key);
+        if (!decodedUser) {
+            return next({
+                staus: 403,
+                error: 'Unauthorized Access',
+                message: 'Unauthorized Access'
+            });
+        }
+        const role = decodedUser.role;
+        if (!hasPermission(module, role, permissionType)) {
+            return next({
+                staus: 403,
+                error: 'Unauthorized Access',
+                message: 'Unauthorized Access'
+            });
+        }
+        next();
     }
-}
+    catch (error) {
+        return next({
+            staus: 403,
+            error: 'Unauthorized Access',
+            message: error.message
+        });
+    }
+};
